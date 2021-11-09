c96eaaf45958b106c723282ede3ae93ddb21f4dd
Ken Reiswig <kreiswig@swagg-book-bro.local>
2021-11-08 20:16:43 -0800

    - add -r flag to specify resolution, default to 1080p - add error conditions if input or output are not provided

--- fabr.sh
+++ fabr.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
-#argIndex = 1;
 audioBitrate="96"
+defaultRes="1920:1080"
 
-while getopts b:i:o: flag
+while getopts i:o:r: flag
+
 do
     case "${flag}" in
         i) input=${OPTARG};;
         o) output=${OPTARG};;
+        r) res=${OPTARG};;
     esac
 done
 
-echo "Input: $input"
-echo "Output $output"
+if [ -z $input ]; then
+    echo "ERROR: no source input was provided"
+    exit 1
+fi
+
+if [ -z $output ]; then
+    echo "ERROR: no output destination was provided"
+    exit 1
+fi
+
+outputRes=${res:-$defaultRes}
 
 for((i = 5; i <=$#; i++)); do
-    bitrateTotalK=$(expr ${!i} + $audioBitrate)
-    ffmpeg -r 60 -stream_loop -1 -i $input -vf scale=640:380 -vcodec libx264 -profile:v baseline -pix_fmt yuv420p -f flv $output
+    ffmpeg -r 60 -stream_loop -1 -i $input -vf scale=$outputRes -vcodec libx264 -profile:v baseline -pix_fmt yuv420p -b:v ${!i}k -b:a 96k -f flv $output
 done
